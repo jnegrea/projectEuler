6df477ec40491b8ef8b0e51535a800617d93edee
jnegrea <jeffrey.negrea@gmail.com>
2016-11-05 13:36:56 -0400

    I didnt solve this one

--- pe0096.hs
+++
@@ -1,7 +0,0 @@
-updateSuDoKu_SimpleDeduction "row" n suDoKu
-	|length (takeWhile (==0) (sort (suDoKu!!n)))==1 = let missingVal = head ([1..9]\\(suDoKu!!n)) in map (\x->if x==0 then missingVal else x) (suDoKu!!n)
-	|otherwise suDoKu
-
-updateSuDoKu_SimpleDeduction "col" n suDoKu = transpose (updateSuDoKu_SimpleDeduction "row" n (transpose suDoKu))
-
-updateSuDoKu_SimpleDeduction "box" n suDoKu = let pseudoRow =[]
